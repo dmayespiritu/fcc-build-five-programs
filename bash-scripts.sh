b1d99c61765137f829cca8247d7f1095d0269d22
Drixie May Espiritu <75099288+dmayespiritu@users.noreply.github.com>
2026-02-09 19:51:18 +0000

    updated all script

--- bash-scripts.sh
+++ bash-scripts.sh
@@ -1,1 +1,86 @@
+# QUESTIONNAIRE PROGRAM SCRIPT #
+
+#!/bin/bash
+
+QUESTION1="What's your name?"
+QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
+
+
+echo -e "\n~~ Questionnaire ~~\n"
+echo $QUESTION1
+read NAME
+echo $QUESTION2
+read LOCATION
+echo $QUESTION3
+read WEBSITE
+
+echo -e "\nHello $NAME from $LOCATION. I learned that your favorite coding website is $WEBSITE!"
+
+# BINGO PROGRAM SCRIPT #
+
+#!/bin/bash
+# Bingo Number Generator
+
+echo -e "\n~~ Bingo Number Generator ~~\n"
+NUMBER=$(( RANDOM % 75 + 1 ))
+TEXT="The next number is, "
+if (( NUMBER <= 15 ))
+then
+    echo $TEXT B:$NUMBER
+elif [[ $NUMBER -le 30 ]]
+then
+    echo $TEXT I:$NUMBER
+elif (( NUMBER < 46 ))
+then
+  echo $TEXT N:$NUMBER
+elif [[ $NUMBER -lt 61 ]]
+then
+  echo $TEXT G:$NUMBER
+else 
+  echo $TEXT O:$NUMBER
+fi
+
+
+# COUNTDOWN PROGRAM SCIPT #
+#!/bin/bash
+# Program that counts down to zero from a given argument
+echo -e "\n~~ Countdown Timer ~~\n"
+if [[ $1 -gt 0 ]]
+then
+ : ' for (( i = $1; i >= 0; i-- ))
+  do
+    echo $i
+    sleep 1
+  done'
+  I=$1
+while [[ $I -ge 0 ]]
+  do echo $I 
+  (( I-- ))
+  sleep 1
+ done 
+else
+  echo Include a positive integer as the first argument.
+fi
+
+# FORTUNE TELLER PROGRAM SCRIPT #
+
+#!/bin/bash
+#Program to tell a persons fortune
+echo -e "\n~~ Fortune Teller ~~\n"
+RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
+N=$(( RANDOM % 6 ))
+
+GET_FORTUNE() {
+  echo Ask a yes or no question:
+  read QUESTION
+}
+
+until [[ $QUESTION == test? ]]
+do
+  GET_FORTUNE
+done
+
+echo ${RESPONSES[$N]}
+
 
